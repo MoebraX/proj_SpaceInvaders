0cecffb6c45fbe49700e4be8b21e20ae84abc30e
MoebraX <ebrahimirad.mohammad31@gmail.com>
2023-06-25 08:50:53 +0330

    initialize the project

---
+++ game.h
@@ -0,0 +1,40 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QGraphicsScene>
+#include "player.h"
+#include <QGraphicsView>
+#include <QTimer>
+#include <QWidget>
+#include "health.h"
+#include "score.h"
+
+class Game : public QGraphicsView
+{
+    Q_OBJECT
+public:
+    Game(QWidget *parent = 0);
+
+    void displayMainmenu();
+    void displayGameover();
+
+    void displaySettingsMenu();
+
+    QGraphicsScene * scene;
+    Player *player;
+    Score *score;
+    Health *health;
+    int sceneWidth;
+    int sceneHeight;
+
+public slots:
+
+
+
+
+};
+
+#endif // GAME_H

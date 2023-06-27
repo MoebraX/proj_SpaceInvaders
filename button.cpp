d09ac9ab28f52aef06f28073a4556ca7db8ebc04
Parham Talebian <137230539+ParhamTalebian@users.noreply.github.com>
2023-06-27 19:11:26 +0330

    button font changed and the background be prettier

--- button.cpp
+++ button.cpp
@@ -1,21 +1,26 @@
 #include "button.h"
 #include <QGraphicsTextItem>
 #include <QBrush>
-
+#include <QFontDatabase>
 Button::Button(QString name, QGraphicsItem *parent): QGraphicsRectItem(parent)
 {
     setRect(0, 0, 200, 50);
     QBrush brush;
     brush.setStyle(Qt::SolidPattern);
-    brush.setColor(Qt::darkCyan);
+    brush.setColor(Qt::black);
     setBrush(brush);
 
-    // draw text
+    // Draw text
     text = new QGraphicsTextItem(name, this);
-    int xPos = rect().width()/2 - text->boundingRect().width()/2;
-    int yPos = rect().height()/2 - text->boundingRect().height()/2;
-    text->setPos(xPos, yPos);
-
+    QFont font;
+    int fontId = QFontDatabase::addApplicationFont(":/fonts/THUNDERSTRIKECOND.TTF");
+    QString fontFamily = QFontDatabase::applicationFontFamilies(fontId).at(0);
+    font.setFamily(fontFamily);
+    font.setPointSize(12); // Adjust the font size to your preference
+    text->setFont(font);
+    text->setDefaultTextColor(Qt::green);
+    text->setPos(rect().width()/2 - text->boundingRect().width()/2,
+                 rect().height()/2 - text->boundingRect().height()/2);
 
     setAcceptHoverEvents(true);
 }
@@ -29,21 +34,20 @@ void Button::hoverEnterEvent(QGraphicsSceneHoverEvent *event)
 {
     QBrush brush;
     brush.setStyle(Qt::SolidPattern);
-    brush.setColor(Qt::cyan);
+    brush.setColor(Qt::darkGreen); // Adjust the color to your preference
     setBrush(brush);
+    QFont font = text->font();
+    font.setPointSize(14); // Increase the font size when hovering
+    text->setFont(font);
 }
 
 void Button::hoverLeaveEvent(QGraphicsSceneHoverEvent *event)
 {
     QBrush brush;
     brush.setStyle(Qt::SolidPattern);
-    brush.setColor(Qt::darkCyan);
+    brush.setColor(Qt::black);
     setBrush(brush);
+    QFont font = text->font();
+    font.setPointSize(12); // Restore the default font size
+    text->setFont(font);
 }
-
-
-
-
-
-
-
